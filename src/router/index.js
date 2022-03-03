044cc6e2c09d98909339f175ccd5c34e799450f4
Work <work@MacBook-Pro-de-Bryan.local>
2022-03-03 12:22:32 +0100

    route arborescence

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 import Rooms from '../views/Rooms.vue'
+import Care from '../views/Care.vue'
+import Catering from '../views/Catering.vue'
 import Events from '../views/Events.vue'
 import Contact from '../views/Contact.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Useraccount from '../views/Useraccount.vue'
 import Booking from '../views/Booking.vue'
-import Admin from '../views/Admin.vue'
 import Timeslot from '../views/Timeslot.vue'
 import Additionaltimeslot from '../views/Additionaltimeslot.vue'
+import Checkouttimeslot from '../views/Checkouttimeslot.vue'
 import Fullday from '../views/Fullday.vue'
 import Additionalfullday from '../views/Additionalfullday.vue'
+import Checkoutfullday from '../views/Checkoutfullday.vue'
+import Faq from '../views/Faq.vue'
+import Privacy from '../views/Privacy.vue'
+import Terms from '../views/Terms.vue'
 
 const routes = [
   {
@@ -19,15 +28,22 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   },
   {
     path: '/rooms',
     name: 'Rooms',
     component: Rooms
+  },
+  {
+    path: '/care',
+    name: 'Care',
+    component: Care
+  },
+  {
+    path: '/catering',
+    name: 'Catering',
+    component: Catering
   },
   {
     path: '/events',
@@ -40,14 +56,19 @@ const routes = [
     component: Contact
   },
   {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/useraccount',
+    name: 'Useraccount',
+    component: Useraccount
+  },
+  {
     path: '/booking',
     name: 'Booking',
     component: Booking
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: Admin
   },
   {
     path: '/timeslot',
@@ -60,6 +81,11 @@ const routes = [
     component: Additionaltimeslot
   },
   {
+    path: '/checkouttimeslot',
+    name: 'Checkouttimeslot',
+    component: Checkouttimeslot
+  },
+  {
     path: '/fullday',
     name: 'Fullday',
     component: Fullday
@@ -68,6 +94,26 @@ const routes = [
     path: '/additionalfullday',
     name: 'Additionalfullday',
     component: Additionalfullday
+  },
+  {
+    path: '/checkoutfullday',
+    name: 'Checkoutfullday',
+    component: Checkoutfullday
+  },
+  {
+    path: '/faq',
+    name: 'Faq',
+    component: Faq
+  },
+  {
+    path: '/privacy',
+    name: 'Privacy',
+    component: Privacy
+  },
+  {
+    path: '/terms',
+    name: 'Terms',
+    component: Terms
   },
 ]
 
