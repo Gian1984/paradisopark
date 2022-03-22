d43525b99ed0e6ac0b975fd467d40384126cb4a1
Gianluca <gl.tiengo@gmail.com>
2022-03-22 16:01:15 +0100

    working on product admin

--- src/components/Additionaloptions.vue
+++ src/components/Additionaloptions.vue
@@ -27,9 +27,59 @@
       </div>
     </div>
   </div>
+
+  <div class="bg-white">
+    <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 class="text-xl font-bold text-gray-900">Customers also bought</h2>
+
+      <div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+        <div v-for="additional in additionals" :key="additional.id">
+          <div class="relative">
+            <div class="relative w-full h-72 rounded-lg overflow-hidden">
+              <img src="https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-01.jpg" class="w-full h-full object-center object-cover" />
+            </div>
+            <div class="relative mt-4">
+              <h3 class="text-sm font-medium text-gray-900">{{ additional.name }}</h3>
+              <p class="mt-1 text-sm text-gray-500">{{ additional.description }}</p>
+            </div>
+            <div class="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
+              <div aria-hidden="true" class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50" />
+              <p class="relative text-lg font-semibold text-white">{{ additional.price }} €</p>
+            </div>
+          </div>
+          <div class="mt-6">
+            <button  type="button" class=" mx-auto relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200"
+            >Modify</button>
+          </div>
+
+
+
+
+
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
+
+
+const products = [
+  {
+    id: 1,
+    name: 'Zip Tote Basket',
+    color: 'White and black',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-01.jpg',
+    imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
+    price: '$140',
+  },
+  // More products...
+]
 
 const tabs = [
   { name: 'My Account', href: '/dashboard', current: false },
@@ -42,22 +92,25 @@ const tabs = [
 export default {
 
   mounted() {
-    this.axios.get(process.env.VUE_APP_URL_API + "api/reservations")
+    this.axios.get(process.env.VUE_APP_URL_API + "api/additionals")
         .then(response => {
-          this.reservations = response.data
+          this.additionals = response.data
         })
   },
 
   data(){
     return{
-      reservations:''
+      additionals:'',
+      isOpen: true
     }
   },
+
 
 
   setup() {
     return {
       tabs,
+      products,
     }
   },
 }
